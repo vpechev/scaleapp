bdfab1f87c7929da5eef1dd21c942f78887b59fe
Vladimir Pechev <vladimir.pechev@scalefocus.com>
2020-03-06 17:10:24 +0200

    Implement dynamically filling & submitting of feedback email

--- web/src/app/notes/notes.component.ts
+++ web/src/app/notes/notes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-notes',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
+  feedbackForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.feedbackForm = this.formBuilder.group({
+      candidateFormControl: new FormControl(''),
+      departmentFormControl: new FormControl(''),
+      feedbackRecipientFormControl: new FormControl(''),
+      feedbackFormControl: new FormControl('')
+    });
+  }
+
+  getMailUri() {
+    let candidateNames = this.feedbackForm.get('candidateFormControl').value;
+    let department = this.feedbackForm.get('departmentFormControl').value;
+    let feedbackRecipient = this.feedbackForm.get('feedbackRecipientFormControl').value;
+    let feedback = this.feedbackForm.get('feedbackFormControl').value;
+
+    let uri = `mailto:${feedbackRecipient}?subject=Feedback for ${candidateNames} for ${department} department&body=${feedback}`;
+    
+    return uri;
   }
 
 }
